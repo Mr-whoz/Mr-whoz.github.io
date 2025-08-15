2ddce1c91fd029dbaa7d6f52720d9c1cba446c3d
Mr-whoz <1124333578@qq.com>
2025-08-15 08:04:38 +0800

    init hope

---
+++ docs/.vuepress/sidebar/zh.ts
@@ -0,0 +1,31 @@
+import { sidebar } from "vuepress-theme-hope";
+
+export const zhSidebar = sidebar({
+  "/": [
+    "",
+    "portfolio",
+    {
+      text: "案例",
+      icon: "laptop-code",
+      prefix: "demo/",
+      link: "demo/",
+      children: "structure",
+    },
+    {
+      text: "文档",
+      icon: "book",
+      prefix: "guide/",
+      children: "structure",
+    },    {
+      text: "广告",
+      icon: "book",
+      prefix: "广告/",
+      children: "structure",
+    },
+    {
+      text: "幻灯片",
+      icon: "person-chalkboard",
+      link: "https://ecosystem.vuejs.press/zh/plugins/markdown/revealjs/demo.html",
+    },
+  ],
+});
